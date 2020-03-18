30ea0c5492cfc93849d4a667f7fa5dc2a564b744
AkhilJose2019 <akhiljose@gmail.com>
2020-03-18 15:59:38 +0530

    updated kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -12,16 +12,9 @@ module.exports = {
         const reason = joinTogetherWords(args, 1, args.length - 1);
         const user = getUserFromMention(args[0], client);
         if (user) {
-            const member = message.guild.member(user);
-            if (member) {
-                member.kick(`for ${reason}`).then(() => {
-                        return `Kicked ${user.username} for ${reason}`;}).catch(err => {
-                        message.reply('Unable to to kick the member');
-                        console.error(err);
-                    });
-            } else {
-                message.reply('The user isn\'t in this server');
-            }
+            
+        } else {
+           return message.reply('give a proper mention or id of the user');
         }
     },
 };
