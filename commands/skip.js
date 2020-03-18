b07ddd48fbccf084754817b240be341f40bba0e5
AkhilJose2019 <akhiljose@gmail.com>
2020-03-18 12:59:57 +0530

    added even response in skip.js

--- commands/skip.js
+++ commands/skip.js
@@ -4,7 +4,9 @@ module.exports = {
     execute(message, args, ytdl) {
         const voiceChannel = message.member.voice.channel;
         if (!voiceChannel) return;
-        
+
+        message.channel.send('**SKipped Music**');
+
         voiceChannel.leave();
     }
 };
