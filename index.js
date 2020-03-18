f4a3cdd516124d907e96a28f1ad4fcaa72935b45
AkhilJose2019 <akhiljose@gmail.com>
2020-03-18 11:25:42 +0530

    removed the voice channel message heander thingy

--- index.js
+++ index.js
@@ -62,22 +62,6 @@ client.once('ready', () => {
     
 });
 
-client.on('message', async message => {
-    if(message.member.voice.channel) {
-        const connection = await message.member.voice.channel.join();
-        const dispatcher = connection.play('./Air.mp3');
-        dispatcher.on('start', () => {
-            console.log('Playing audio');
-        });
-        
-        dispatcher.on('finish', () => {
-            console.log('Finished playing'); 
-        });
-        
-        dispatcher.on('error', console.error);
-    }
-});
-
 client.on('guildMemberAdd', member => {
     const channel = member.guild.channels.cache.find(ch => ch.name === 'member-log');
     if (!channel) return;
